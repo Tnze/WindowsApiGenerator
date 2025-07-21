6b0eb760a199a2e665709e3e4619c24c28c59b32
Manuel Bl. <manuel.bleichenbacher@gmail.com>
2025-07-21 19:19:12 +0200

    Maven plugin: delete events are logged

--- windowsapi-maven-plugin/src/main/java/net/codecrete/windowsapi/maven/EventLogger.java
+++ windowsapi-maven-plugin/src/main/java/net/codecrete/windowsapi/maven/EventLogger.java
@@ -12,6 +12,8 @@ import net.codecrete.windowsapi.events.Event.JavaSourceGenerated;
 import net.codecrete.windowsapi.events.EventListener;
 import org.apache.maven.plugin.logging.Log;
 
+import java.nio.file.Path;
+
 /**
  * Event listener writing to messages to the Maven logger.
  */
@@ -27,6 +29,7 @@ public class EventLogger implements EventListener {
         this.logger = logger;
     }
 
+    @SuppressWarnings("java:S6916")
     @Override
     public void onEvent(Event event) {
         switch (event) {
@@ -37,6 +40,14 @@ public class EventLogger implements EventListener {
             case DirectoryCreated(var path) -> {
                 if (logger.isDebugEnabled())
                     logger.debug("Created source directory " + path);
+            }
+            case Event.FileDeleted(Path path) -> {
+                if (logger.isDebugEnabled())
+                    logger.debug("Deleted file " + path);
+            }
+            case Event.DirectoryDeleted(Path path) -> {
+                if (logger.isDebugEnabled())
+                    logger.debug("Deleted directory " + path);
             }
             case Event.InvalidArgument(var argument, var value, var reason)
                     -> logger.error(String.format("Invalid value '%s' for argument %s: %s", value, argument, reason));
